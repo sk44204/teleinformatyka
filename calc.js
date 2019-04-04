cfab4f39afc7164fc9d0cc92cbaadabe091c9720
sk44204 <procyon.lotor@wp.pl>
2019-04-04 13:40:26 +0200

    kalkulator js

--- calc.js
+++ calc.js
@@ -1,9 +1,44 @@
-const deg2rad = deg => deg * Math.PI/180;
-const init = (ev,a) => {
-console.log('Container:',document.getElementById('container'));
-console.log(ev);
-console.log(deg2rad(a));
-console.log('Children:',document.getElementById('container').children);
+const fields = [
+    {txt:1,col:1, row:4},
+    {txt:2,col:2, row:4},
+    {txt:3,col:3, row:4},
+    {txt:4,col:1, row:3},
+    {txt:5,col:2, row:3},
+    {txt:6,col:3, row:3},
+    {txt:7,col:1, row:2},
+    {txt:8,col:2, row:2},
+    {txt:9,col:3, row:2},
+    {txt:0,col:'1/3', row:5},
+    {txt:'=',col:4, row:5},
+    {txt:'.',col:3, row:5},
+    {txt:'C',col:4, row:2},
+    {txt:'+',col:4, row:3},
+    {txt:'-',col:4, row:4},
+    {txt:'Display',col:'1/5', row:1},
+];
+
+const init = () => {
+
+    const container = document.createElement('div');
+    container.id='container'
+
+    fields.forEach(el => {
+        const button = document.createElement('div')
+        button.textContent = el.txt;
+        button.style.gridColumn = el.col;
+        button.style.gridRow = el.row;
+        if(el.txt === 'Display'){
+            button.id='display';
+        } else {
+            button.addEventListener('click',ev => {
+                const d = document.getElementById('display');
+                d.textContent = ev.target.textContent;
+                ev.target.textContent='#';
+            });
+        }
+        container.appendChild(button);
+    });
+    document.body.appendChild(container);
 }
 
-window.addEventListener('DOMContentLoaded', x => init(x,90));
+window.addEventListener('DOMContentLoaded', init);
